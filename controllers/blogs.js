52d33f9199b7349b5f717b310fa6d42f7e34a5fa
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2023-09-12 16:27:12 +0300

    Revert "4.14"
    This reverts commit 137ff103db9ec83f050b6e39fe07403a0cc6e46d.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,53 +21,71 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const likes = body.likes !== undefined ? body.likes : 0;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: likes
+  })
+if (body.title !== undefined && body.author !== undefined) {
+  blog.save()
+    .then(savedBlog => {
+      response.status(201).json(savedBlog)
+      
+    })
+    .catch(error => next(error))
+  } else {
+    response.sendStatus(400).send('Add Title and Url');
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    const body = request.body;
-    
-    if (!body.title || !body.author) {
-      return response.status(400).json({ error: 'Title and author are required fields' });
+    const blogId = request.params.id;
+    const deletedBlog = await Blog.findByIdAndRemove(blogId);
+
+    if (deletedBlog) {
+      response.status(204).end(); // Return 204 No Content on successful deletion
+    } else {
+      response.status(404).end(); // Return 404 Not Found if blog was not found
     }
-    
-    const user = await User.findById(body.userId);
-    const likes = body.likes !== undefined ? body.likes : 0;
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: likes,
-      user: user._id
-    });
-
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
   } catch (error) {
     next(error);
   }
 });
 
-blogsRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body;
 
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: likes,
-      user: user._id
-    });
+  const updatedBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
+  try {
+    const blogId = request.params.id;
+    const updatedBlogDocument = await Blog.findByIdAndUpdate(
+      blogId,
+      updatedBlog,
+      { new: true }
+    );
 
-blogsRouter.delete(':/id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
+    if (updatedBlogDocument) {
+      response.json(updatedBlogDocument);
+    } else {
+      response.status(404).end(); 
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = blogsRouter
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -22,30 +22,6 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(initialBlogs.length);
 });
 
-test('a valid blog can be added ', async () => {
-  const newNote =  {
-    title: 'Hei',
-    author: 'Miska',
-    url: 'https://example.com/blog3',
-    likes: 2,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newNote)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-
-  const contents = response.body.map(r => r.title);
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(contents).toContain(
-    'Hei'
-  )
-})
-
 test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs');
   const titles = response.body.map((r) => r.title);
@@ -121,60 +97,6 @@ test('deleting a blog by ID succeeds with status code 204', async () => {
 
   expect(response.status).toBe(204);
   expect(ids).not.toContain(blogToDelete.id);
-});
-
-describe('when there is initially one user at db', () => {
-  beforeEach(async () => {
-    await User.deleteMany({});
-
-    const passwordHash = await bcrypt.hash('sekret', 10);
-    const user = new User({ username: 'root', passwordHash });
-
-    await user.save();
-  });
-
-  test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await helper.usersInDb();
-
-    const newUser = {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen',
-      password: 'salainen',
-    };
-
-    const response = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-
-    const usersAtEnd = await helper.usersInDb();
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
-
-    const usernames = usersAtEnd.map((user) => user.username);
-    expect(usernames).toContain(newUser.username);
-  });
-
-  test('creation fails with proper status code and message if username already taken', async () => {
-    const usersAtStart = await helper.usersInDb();
-
-    const newUser = {
-      username: 'root',
-      name: 'Superuser',
-      password: 'salainen',
-    };
-
-    const response = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/);
-
-    expect(response.body.error).toContain('expected `username` to be unique');
-
-    const usersAtEnd = await helper.usersInDb();
-    expect(usersAtEnd).toHaveLength(usersAtStart.length);
-  });
 });
 
 afterAll(async () => {
