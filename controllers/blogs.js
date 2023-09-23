25508a153f829b8ed7a27933c1580ecb2d6ddce1
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2023-09-23 14:20:19 +0300

    Fixes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,85 +1,88 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const jwt = require('jsonwebtoken');
-require('express-async-errors')
-const User = require('../models/user');
-const blog = require('../models/blog');
-const { extractUser } = require('../utils/middleware');
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const jwt = require("jsonwebtoken");
+require("express-async-errors");
+const User = require("../models/user");
+const blog = require("../models/blog");
+const { extractUser } = require("../utils/middleware");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response, next) => {
-  const blogs = await Blog.findById(request.params.id)
+blogsRouter.get("/:id", async (request, response, next) => {
+  const blogs = await Blog.findById(request.params.id);
   if (blogs) {
-    response.json(blogs)
+    response.json(blogs);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-blogsRouter.post('/', async (request, response, next) => {
-  const body = request.body
+blogsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+    return response.status(401).json({ error: "token invalid" });
   }
-  const user = request.user
+  const user = request.user;
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes !== undefined ? body.likes : 0,
-    user: user.id
-  })
-if (body.title !== undefined && body.author !== undefined) {
-  const savedBlog = await blog.save()
-  console.log(savedBlog)
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+    user: user._id,
+  });
+  if (body.title && body.author && body.url) {
+    const savedBlog = await blog.save();
+    console.log(user._id);
+    console.log(savedBlog);
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-  response.json(savedBlog)
+    response.json(savedBlog);
   } else {
-    response.sendStatus(400).send('Add Title and Url');
+    response.sendStatus(400).send("Add Title and Url");
   }
-})
+});
 
-blogsRouter.delete('/:id', extractUser ,async (request, response, next) => {
+blogsRouter.delete("/:id", extractUser, async (request, response, next) => {
   try {
-    const token = request.token
+    const token = request.token;
     const blogId = request.params.id;
 
     if (!token) {
-      response.status(401).json({ error: "Token not found"})
+      response.status(401).json({ error: "Token not found" });
     }
 
     const deletedBlog = await Blog.findById(blogId);
 
     if (!deletedBlog) {
-      response.status(404).json({ error: "Blog not found"})
+      response.status(404).json({ error: "Blog not found" });
     }
 
-    const user = request.user
+    const user = request.user;
 
     if (user.toString() === user._id.toString()) {
-      await blog.findByIdAndRemove(blogId)
+      await blog.findByIdAndRemove(blogId);
 
-      response.status(204).end()
+      response.status(204).end();
     } else {
-      response.status(403).json({ error: "You dont have permission to delete this blod"})
-      console.log(user.toString())
-      console.log(user.id.toString())
+      response
+        .status(403)
+        .json({ error: "You dont have permission to delete this blod" });
+      console.log(user.toString());
+      console.log(user.id.toString());
     }
   } catch (error) {
     next(error);
   }
 });
 
-blogsRouter.put('/:id', async (request, response, next) => {
+blogsRouter.put("/:id", async (request, response, next) => {
   const body = request.body;
 
   const updatedBlog = {
@@ -100,12 +103,11 @@ blogsRouter.put('/:id', async (request, response, next) => {
     if (updatedBlogDocument) {
       response.json(updatedBlogDocument);
     } else {
-      response.status(404).end(); 
+      response.status(404).end();
     }
   } catch (error) {
     next(error);
   }
 });
 
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,70 +1,70 @@
-const { request } = require('http')
-const logger = require('./logger')
-const { response } = require('../app')
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const { request } = require("http");
+const logger = require("./logger");
+const { response } = require("../app");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
+  logger.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  } else if (error.name === 'JsonWebTokenError') {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({
-      error: 'invalid token'
-    })
-  } else if (error.name === 'TokenExpiredError') {
+      error: "invalid token",
+    });
+  } else if (error.name === "TokenExpiredError") {
     return response.status(401).json({
-      error: 'token expired'
-    })
+      error: "token expired",
+    });
   }
 
-  next(error)
-}
+  next(error);
+};
 
 const extractToken = (request, response, next) => {
   const authHeader = request.headers.authorization;
 
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    const token = authHeader.split(' ')[1];
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
     request.token = token;
   }
 
-  next(); 
-}
+  next();
+};
 
 const extractUser = async (request, response, next) => {
-  const token = request.token; 
+  const token = request.token;
 
   if (!token) {
-    return response.status(401).json({ error: 'Token missing' });
+    return response.status(401).json({ error: "Token missing" });
   }
 
   try {
-    const decodedToken = jwt.verify(token, process.env.SECRET); 
+    const decodedToken = jwt.verify(token, process.env.SECRET);
     if (!decodedToken.id) {
-      return response.status(401).json({ error: 'Token invalid' });
+      return response.status(401).json({ error: "Token invalid" });
     }
     const user = await User.findById(decodedToken.id);
 
     if (!user) {
-      return response.status(401).json({ error: 'User not found' });
+      return response.status(401).json({ error: "User not found" });
     }
-    request.user = user._id;
+    request.user = user;
 
     next();
   } catch (error) {
@@ -77,5 +77,5 @@ module.exports = {
   unknownEndpoint,
   errorHandler,
   extractToken,
-  extractUser
-}
+  extractUser,
+};
